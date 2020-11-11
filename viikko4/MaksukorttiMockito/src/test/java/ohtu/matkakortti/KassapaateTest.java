0c4d9322609db0c35e0289a6321620e06f6788d6
Lassi Laiho <lassi.laiho0@gmail.com>
2020-11-11 17:46:40 +0200

    Fix Kassapääte class

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -13,21 +13,21 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KassapaateTest {
-    
+
     Kassapaate kassa;
     Maksukortti kortti;
-    
+
     @Before
     public void setUp() {
         kassa = new Kassapaate();
         kortti = mock(Maksukortti.class);
     }
-    
+
     @Test
     public void kortiltaVelotetaanHintaJosRahaaOn() {
         when(kortti.getSaldo()).thenReturn(10);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti).osta(eq(Kassapaate.HINTA));
     }
@@ -36,9 +36,20 @@ public class KassapaateTest {
     public void kortiltaEiVelotetaJosRahaEiRiita() {
         when(kortti.getSaldo()).thenReturn(4);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
+    @Test
+    public void lataaKortilleJosRahamääräPositiivinen() {
+        kassa.lataa(kortti, 5);
+        verify(kortti).lataa(eq(5));
+    }
+
+    @Test
+    public void äläLataaKortilleJosRahamääräNegatiivinen() {
+        kassa.lataa(kortti, -5);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
 }
